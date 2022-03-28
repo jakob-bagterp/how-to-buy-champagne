11d3f54dc6f0d32bb0b17954116915d7b180a075
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-28 23:28:28 +0200

    Add first draft of main flow.

---
+++ buy_champagne.py
@@ -0,0 +1,10 @@
+from config.browser_extension import Browser
+from config.cookie_banner import ACCEPT_COOKIES
+from config.shopping_list import POL_COURONNE_BRUT
+
+if __name__ == "__main__":
+    with Browser() as browser:
+        browser.open.url("https://www.honestgrapes.co.uk/")
+        browser.combo.cookie_banner(ACCEPT_COOKIES)
+        browser.buy(10, POL_COURONNE_BRUT)
+        browser.check_out()
