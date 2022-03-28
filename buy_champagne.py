377eefc0a90929ec959e6f28baa1d03c9204a400
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-03-28 23:50:30 +0200

    Add extra items to shopping list and main flow.

--- buy_champagne.py
+++ buy_champagne.py
@@ -1,10 +1,14 @@
 from config.browser_extension import Browser
 from config.cookie_banner import ACCEPT_COOKIES
-from config.shopping_list import POL_COURONNE_BRUT
+from config.shopping_list import (BILLECART_SALMON_BRUT_RESERVE, FREREJEAN_FRERES_BLANC_DE_BLANCS,
+                                  KRUG_ROSE_20TH_EDITION_MAGNUM, POL_COURONNE_BRUT)
 
 if __name__ == "__main__":
     with Browser() as browser:
         browser.open.url("https://www.honestgrapes.co.uk/")
         browser.combo.cookie_banner(ACCEPT_COOKIES)
-        browser.add_to_cart(10, POL_COURONNE_BRUT)
+        browser.add_to_cart(20, POL_COURONNE_BRUT)
+        browser.add_to_cart(10, FREREJEAN_FRERES_BLANC_DE_BLANCS)
+        browser.add_to_cart(20, BILLECART_SALMON_BRUT_RESERVE)
+        browser.add_to_cart(20, KRUG_ROSE_20TH_EDITION_MAGNUM)
         browser.check_out()
--- config/shopping_list.py
+++ config/shopping_list.py
@@ -3,3 +3,15 @@ from .model import Item
 POL_COURONNE_BRUT = Item(
     url="https://www.honestgrapes.co.uk/pol-couronne-brut-nv"
 )
+
+FREREJEAN_FRERES_BLANC_DE_BLANCS = Item(
+    url="https://www.honestgrapes.co.uk/champagne-frerejean-freres-blanc-de-blancs"
+)
+
+BILLECART_SALMON_BRUT_RESERVE = Item(
+    url="https://www.honestgrapes.co.uk/billecart-salmon-brut-reserve"
+)
+
+KRUG_ROSE_20TH_EDITION_MAGNUM = Item(
+    url="https://www.honestgrapes.co.uk/krug-rose-20th-edition-gift-pack"
+)
